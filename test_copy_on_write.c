f127b9ee371176ddc30a55a6205becc2fd4acc8c
Brandon Abutin <babut001@ucr.edu>
2020-03-04 15:51:19 -0800

    Adjust test case

--- test_copy_on_write.c
+++ test_copy_on_write.c
@@ -2,19 +2,26 @@
 #include "user.h"
 #include "stat.h"
 
+int val = 0;
+
 int main(void) {
-  int val = 0;
   int pid;
   pid = fork();
   if(pid == 0) {
     printf(1, "Number of free pages in child: %d\n", getNumberOfFreePages());
     val = 1;
     printf(1, "number of free pages in child after changing: %d\n", getNumberOfFreePages());
-    if(val == 1)
-      printf(1, "Number of free pages in child after changing variable: %d\n", getNumberOfFreePages());
   } else {
-    printf(1, "Number of free pages in parent: %d\n", getNumberOfFreePages());
     wait();
+    pid = fork();
+    if(pid == 0) {
+      printf(1, "Number of free pages in child2: %d\n", getNumberOfFreePages());
+      val = 1;
+      printf(1, "Number of free pages in child 2 after changing: %d\n", getNumberOfFreePages());    
+    } else {
+      printf(1, "Number of free pages in parent: %d\n", getNumberOfFreePages());
+      wait();
+    }
   }
   exit();
 }
