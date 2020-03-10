2d40da5a7147d86435e9a41bd37652b4348a9df1
Brandon Abutin <babut001@ucr.edu>
2020-03-10 15:06:27 -0700

    implement pseudorandom function

--- exec.c
+++ exec.c
@@ -6,8 +6,21 @@
 #include "defs.h"
 #include "x86.h"
 #include "elf.h"
-#include "stdlib.h"
-#include "time.h"
+
+uint random(void) {
+  static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+  unsigned int b;
+  b  = ((z1 << 6) ^ z1) >> 13;
+  z1 = ((z1 & 4294967294U) << 18) ^ b;
+  b  = ((z2 << 2) ^ z2) >> 27; 
+  z2 = ((z2 & 4294967288U) << 2) ^ b;
+  b  = ((z3 << 13) ^ z3) >> 21;
+  z3 = ((z3 & 4294967280U) << 7) ^ b;
+  b  = ((z4 << 3) ^ z4) >> 12;
+  z4 = ((z4 & 4294967168U) << 13) ^ b;
+
+  return (z1 ^ z2 ^ z3 ^ z4) / 2;
+}
 
 int
 exec(char *path, char **argv)
@@ -63,7 +76,7 @@ exec(char *path, char **argv)
   ip = 0;
 
   srand(time(0));
-  sz = PGROUNDUP(sz) + ((rand() % (65536 + 1)) * PGSIZE);
+  sz = PGROUNDUP(sz) + ((random() % (65536 + 1)) * PGSIZE);
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
   clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
