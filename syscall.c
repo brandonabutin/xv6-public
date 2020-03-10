65454d3162fa8330147a3b3f46d15ff4786354b7
Brandon Abutin <babut001@ucr.edu>
2020-03-10 16:43:56 -0700

    Fix syscall tests broken by moving stack

--- syscall.c
+++ syscall.c
@@ -19,8 +19,8 @@ fetchint(uint addr, int *ip)
 {
   struct proc *curproc = myproc();
 
-  //if(addr >= curproc->sz || addr+4 > curproc->sz)
-  //  return -1;
+  if(addr >= curproc->stacklocation || addr+4 > curproc->stacklocation)
+    return -1;
   *ip = *(int*)(addr);
   return 0;
 }
@@ -34,10 +34,10 @@ fetchstr(uint addr, char **pp)
   char *s, *ep;
   struct proc *curproc = myproc();
 
-  //if(addr >= curproc->sz)
-  //  return -1;
+  if(addr >= curproc->stacklocation)
+    return -1;
   *pp = (char*)addr;
-  ep = (char*)curproc->sz;
+  ep = (char*)curproc->stacklocation;
   for(s = *pp; s < ep; s++){
     if(*s == 0)
       return s - *pp;
@@ -63,8 +63,8 @@ argptr(int n, char **pp, int size)
  
   if(argint(n, &i) < 0)
     return -1;
-  //if(size < 0 || (uint)i >= curproc->sz || (uint)i+size > curproc->sz)
-  //  return -1;
+  if(size < 0 || (uint)i >= curproc->stacklocation || (uint)i+size > curproc->stacklocation)
+    return -1;
   *pp = (char*)i;
   return 0;
 }
